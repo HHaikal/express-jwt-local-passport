8b28d6eb43d6f1cb92245c1421217d505392df6d
vederick <haikalhikmi13@gmail.com>
2019-01-28 00:24:30 +0800

    Port
    Signed-off-by: vederick <haikalhikmi13@gmail.com>

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.get('/', (req, res, next) => {
 
 // /* --- */
 
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 app.listen(PORT, () => {
     console.log(`Mixing it up on port ${PORT}`)
 })
