b45cc0a3c69fda4fc69a24339fc5b72697ad4b5d
vederick <haikalhikmi13@gmail.com>
2019-01-28 00:08:44 +0800

    testing
    Signed-off-by: vederick <haikalhikmi13@gmail.com>

--- index.js
+++ index.js
@@ -1,112 +1,29 @@
 const express = require('express')
-const app = express()
-const port = 5000;
-
-const jwt = require('jsonwebtoken')
-const passport = require('passport')
-const passportJwt = require('passport-jwt')
-
-var ExtractJwt = passportJwt.ExtractJwt;
-var JwtStrategy = passportJwt.Strategy;
-
-const _ = require('lodash')
-const bodyParser = require('body-parser')
-
+const cowsay = require('cowsay')
 const cors = require('cors')
-
-/* --- PART: initialize */
-app.use(passport.initialize())
-app.use(cors())
-
-// parse application/x-www-form-urlencoded
-// for easier testing with Postman or plain HTML forms
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-// parse application/json
-app.use(bodyParser.json())
-/* --- */
-
-/* --- PART: Strategy */
-const jwtOptions = {}
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = '8shots'
-
-
-const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-    console.log('payload received', jwt_payload);
-    // usually this would be a database call:
-    var user = users[_.findIndex(users, { id: jwt_payload.id })];
-    if (user) {
-        next(null, user);
-    } else {
-        next(null, false);
-    }
-});
-
-passport.use(strategy);
-
-passport.use(strategy)
-/* --- */
-
-/* ---- PART: Fake Users */
-const users = [
-    {
-        id: 1,
-        name: 'Some Name 1',
-        email: 'someemail1@gmail.com',
-        password: 'magnificentSamarinda'
-    },
-    {
-        id: 2,
-        name: 'Some Name 2',
-        email: 'someemail2@gmail.com',
-        password: 'magnificentSamarinda'
-    }
-]
-/* ---- */
-
-
-/* --- PART: route */
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'express is up'
-    })
-})
-
-app.post('/login', (req, res, next) => {
-    const email = req.body.email
-    const password = req.body.password
-
-    const user = users[_.findIndex(users, { email: email })]
-    if (!user) {
-        res.sendStatus(403)
-    }
-
-    if (user.password === password) {
-        // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-        var payload = { id: user.id };
-        var token = jwt.sign(payload, jwtOptions.secretOrKey);
-        res.json({ message: "ok", token: token });
-    } else {
-        res.sendStatus(403)
+// Create the server
+const app = express()
+// Serve our api route /cow that returns a custom talking text cow
+app.get('/api/cow/:say', cors(), async (req, res, next) => {
+    try {
+        const text = req.params.say
+        const moo = cowsay.say({ text })
+        res.json({ moo })
+    } catch (err) {
+        next(err)
     }
 })
-
-app.get("/secret", passport.authenticate('jwt', { session: false }), function (req, res) {
-    res.json({ message: "Success! You can not see this without a token" });
-});
-
-app.get("/secretDebug",
-    function (req, res, next) {
-        console.log(req.get('Authorization'));
-        next();
-    }, function (req, res) {
-        res.json("debugging");
-    });
-
-/* --- */
-
-app.listen(port, () => console.log(`express running in port ${port}`))
+// Serve our base route that returns a Hello World cow
+app.get('/api/cow/', cors(), async (req, res, next) => {
+    try {
+        const moo = cowsay.say({ text: 'Hello World!' })
+        res.json({ moo })
+    } catch (err) {
+        next(err)
+    }
+})
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Mixing it up on port ${PORT}`)
+})
