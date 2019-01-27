9aa08a4faf2d387dd1ee952164412b461eb20645
vederick <haikalhikmi13@gmail.com>
2019-01-28 00:32:50 +0800

    Port
    Signed-off-by: vederick <haikalhikmi13@gmail.com>

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express()
 
-// const jwt = require('jsonwebtoken')
-// const passport = require('passport')
-// const passportJwt = require('passport-jwt')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
 const cowsay = require('cowsay')
 
-// var ExtractJwt = passportJwt.ExtractJwt;
-// var JwtStrategy = passportJwt.Strategy;
+var ExtractJwt = passportJwt.ExtractJwt;
+var JwtStrategy = passportJwt.Strategy;
 
-// const _ = require('lodash')
+const _ = require('lodash')
 const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
 /* --- PART: initialize */
-// app.use(passport.initialize())
+app.use(passport.initialize())
 app.use(cors())
 
 // parse application/x-www-form-urlencoded
@@ -29,23 +29,22 @@ app.use(bodyParser.json())
 /* --- */
 
 /* --- PART: Strategy */
-// const jwtOptions = {}
-// jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// jwtOptions.secretOrKey = '8shots'
+const jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = '8shots'
 
+const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    // usually this would be a database call:
+    var user = users[_.findIndex(users, { id: jwt_payload.id })];
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
 
-// const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-//     console.log('payload received', jwt_payload);
-//     // usually this would be a database call:
-//     var user = users[_.findIndex(users, { id: jwt_payload.id })];
-//     if (user) {
-//         next(null, user);
-//     } else {
-//         next(null, false);
-//     }
-// });
-
-// passport.use(strategy);
+passport.use(strategy);
 /* --- */
 
 /* ---- PART: Fake Users */
@@ -77,40 +76,40 @@ app.get('/', (req, res, next) => {
     }
 })
 
-// app.post('/login', (req, res, next) => {
-//     const email = req.body.email
-//     const password = req.body.password
+app.post('/login', (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
 
-//     const user = users[_.findIndex(users, { email: email })]
-//     if (!user) {
-//         res.sendStatus(403)
-//     }
+    const user = users[_.findIndex(users, { email: email })]
+    if (!user) {
+        res.sendStatus(403)
+    }
 
-//     if (user.password === password) {
-//         // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-//         var payload = { id: user.id };
-//         var token = jwt.sign(payload, jwtOptions.secretOrKey);
-//         res.json({ message: "ok", token: token });
-//     } else {
-//         res.sendStatus(403)
-//     }
-// })
+    if (user.password === password) {
+        // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+        var payload = { id: user.id };
+        var token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({ message: "ok", token: token });
+    } else {
+        res.sendStatus(403)
+    }
+})
 
-// app.get("/secret", passport.authenticate('jwt', { session: false }), function (req, res) {
-//     res.json({ message: "Success! You can not see this without a token" });
-// });
+app.get("/secret", passport.authenticate('jwt', { session: false }), function (req, res) {
+    res.json({ message: "Success! You can not see this without a token" });
+});
 
-// app.get("/secretDebug",
-//     function (req, res, next) {
-//         console.log(req.get('Authorization'));
-//         next();
-//     }, function (req, res) {
-//         res.json("debugging");
-//     });
+app.get("/secretDebug",
+    function (req, res, next) {
+        console.log(req.get('Authorization'));
+        next();
+    }, function (req, res) {
+        res.json("debugging");
+    });
 
 // /* --- */
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Mixing it up on port ${PORT}`)
 })
